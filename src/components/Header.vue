cad5a43ef381906b44dde40ac97370ace0c63a0f
davydocsurg <davydocsurg@gmail.com>
2020-04-27 01:28:43 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+    <b-nav tabs>
+        <b-nav-item disabled><b>Quiz app</b></b-nav-item>
+        <b-nav-item disabled><b>Counter: {{ numCorrect }}/{{ numTotal }}</b></b-nav-item>
+    </b-nav>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'numCorrect',
+        'numTotal'
+    ]
+}
+</script>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,120 @@
+<template>
+<div>
+  <b-jumbotron>
+
+    <template v-slot:lead>
+        {{currentQuestion.question}}
+    </template>
+
+    <hr class="my-4">
+        <b-list-group>
+            <b-list-group-item v-for="(answer, index) in answers"
+            @click="selectAnswer(index)" :key="index"
+            :class="[
+                !answered && selectedIndex === index ? 'selected' :
+                answered && correctIndex === index ? 'correct' : '']">
+                  {{ answer }}
+            </b-list-group-item>
+        </b-list-group>
+
+    <b-button variant="primary" class="btn"
+    @click="submitAnswer" :disabled="selectedIndex === null || answered">
+        Submit
+    </b-button>
+    <b-button @click="next" variant="success" class="btn">Next</b-button>
+  </b-jumbotron>
+</div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+    props: {
+        currentQuestion: Object,
+        next: Function,
+        increment: Function,
+    },
+    data() {
+        return {
+            selectedIndex: null,
+            correctIndex: null,
+            shuffledAnswers: [],
+            answered: false
+        }
+    },
+    computed: {
+        answers(){
+            let answers=[...this.currentQuestion.incorrect_answers]
+            answers.push(this.currentQuestion.correct_answer)
+            return answers
+        }
+    },
+    watch: {
+        currentQuestion: {
+            immediate: true,
+            handler() {
+                this.selectedIndex = null
+                this.answered = false
+                this.shuffleAnswers()
+            }
+        }
+
+        // (){
+        //     this.selectedIndex = null
+        //     this.shuffleAnswers()
+        // }
+    },
+    methods: {
+        selectAnswer(index) {
+            this.selectedIndex = index
+        },
+        submitAnswer() {
+            let isCorrect = false
+
+            if (this.selectedIndex === this.correctIndex){
+                isCorrect = true
+            }
+            this.answered = true
+
+            this.increment(isCorrect)
+        },
+        shuffleAnswers(){
+            let answers=[...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+            // lodash imported as an underscore
+            this.shuffledAnswers = _.shuffle(answers)
+            this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+        }
+    },
+    // mounted() {
+    //     console.log(this.currentQuestion)
+    // },
+}
+</script>
+
+<style scoped>
+    .list-group{
+        margin-bottom: 15px;
+    }
+
+    .list-group-item:hover{
+        background: #eee;
+        cursor: pointer;
+    }
+
+    .btn {
+        margin: 0 5px;
+    }
+
+    .selected {
+        background-color: lightblue;
+    }
+
+    .correct {
+        background-color: lightgreen;
+    }
+
+    .incorrect {
+        background-color: red;
+    }
+</style>
